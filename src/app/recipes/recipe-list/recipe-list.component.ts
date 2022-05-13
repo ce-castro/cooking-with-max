f5be7be0392d8f50e2b7e728ba8e625e35e0beeb
Cesar Castro <ce.castro@outlook.es>
2022-05-12 18:16:56 -0600

    Beginning of section 6

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Just another test recipe', 'https://cdn.shopify.com/s/files/1/0280/0130/4716/files/gnubees-banana-chocolate-muffins_480x480.jpg?v=1617910866'),
+    new Recipe('Test recipe', 'Just another test recipe', 'https://cdn.shopify.com/s/files/1/0280/0130/4716/files/gnubees-banana-chocolate-muffins_480x480.jpg?v=1617910866')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
