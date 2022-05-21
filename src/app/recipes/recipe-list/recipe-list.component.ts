e97781e4cfed5338888153f88fd2e8d79e37f9cd
Cesar Castro <ce.castro@outlook.es>
2022-05-21 11:17:13 -0600

    Almost the end of the chapter

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.module';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'Just another test recipe', 'https://cdn.shopify.com/s/files/1/0280/0130/4716/files/gnubees-banana-chocolate-muffins_480x480.jpg?v=1617910866'),
-    new Recipe('Test recipe', 'Just another test recipe', 'https://cdn.shopify.com/s/files/1/0280/0130/4716/files/gnubees-banana-chocolate-muffins_480x480.jpg?v=1617910866')
+    new Recipe('Another Test recipe', 'Just another test recipe', 'https://cdn.shopify.com/s/files/1/0280/0130/4716/files/gnubees-banana-chocolate-muffins_480x480.jpg?v=1617910866')
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
